4044d1ffc7714b456781e9a0ef9bb5fa80c3663d
O'Ryan <oryanalexander_1@hotmail.com>
2020-07-08 21:08:35 -0400

    feat: added a printQuote function and setInterval

--- js/script.js
+++ js/script.js
@@ -13,34 +13,34 @@ project 1 - A Random Quote Generator
 
 const quotes = [
   {
-    quote: "1", //the actual quote
-    source: "", //the person or character who said it
+    quote:
+      "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.", //the actual quote
+    source: "Winston Churchill", //the person or character who said it
     citation: "", //the source of the quote, like the book, movie or song where the quote originates.
     year: "" // When the quote was created.
   },
   {
-    quote: "2",
-    source: "",
-    citation: "",
+    quote:
+      "Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough.",
+    source: "Og Mandino"
+  },
+  {
+    quote: "We Generate Fears While We Sit. We Overcome Them By Action.",
+    source: "Dr. Henry Link"
+  },
+  {
+    quote:
+      "Security Is Mostly A Superstition. Life Is Either A Daring Adventure Or Nothing.",
+    source: "Helen Keller",
+    citation: "Life Quote",
     year: ""
   },
   {
-    quote: "3",
-    source: "",
+    quote:
+      "The two most important days in your life are the day you are born and the day you find out why.",
+    source: "Mark Twain",
     citation: "",
-    year: ""
-  },
-  {
-    quote: "4",
-    source: "",
-    citation: "",
-    year: ""
-  },
-  {
-    quote: "5",
-    source: "",
-    citation: "",
-    year: ""
+    year: "2018"
   }
 ];
 
@@ -59,13 +59,32 @@ console.log(getRandomQuote());
 
 /***
  * `printQuote` function
+ * This function will print a random qutoe to the screen.
  ***/
-function printQuote() {}
+function printQuote() {
+  const div = document.getElementById("quote-box");
+  const getQuotes = getRandomQuote();
+  let html = `<p class="quote">${getQuotes.quote}</p>
+  <p class="source"> ${getQuotes.source}`;
+
+  if (getQuotes.citation) {
+    html += `<span class="citation"> ${getQuotes.citation}</span>`;
+  }
+
+  if (getQuotes.year) {
+    html += `<span class="year"> ${getQuotes.year}</span></p>`;
+  }
+
+  return (div.innerHTML = html);
+}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
  ***/
+
+//  Every 5 seconds a new qutoe will display.
+setInterval(printQuote, 5000);
 
 document
   .getElementById("load-quote")
